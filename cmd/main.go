bd53823de878ffdce28323083b66fdbb6d0f908c
Khanbala Rashidov <50279392+KhanbalaRashidov@users.noreply.github.com>
2024-07-20 21:47:00 +0400

    Create handler

--- cmd/main.go
+++ cmd/main.go
@@ -1,15 +1,17 @@
 package main
 
 import (
+	"github.com/KhanbalaRashidov/go-restapi/pkg/handler"
 	"log"
 
 	gorestapi "github.com/KhanbalaRashidov/go-restapi"
 )
 
 func main() {
+	handlers := new(handler.Handler)
 	srv := new(gorestapi.Server)
 
-	if err := srv.Run("8000"); err != nil {
+	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
 		log.Fatalf("error ocurred  while  running  http  server: %s", err.Error())
 	}
 }
